805ab0f8cac7d3da9f5b4082493d5265ddfc3309
tejaskasare14 <tejaskasare.projects@gmail.com>
2023-11-04 11:33:01 +0530

    js datatype added

---
+++ MERN/Javascript/datatype.js
@@ -0,0 +1,46 @@
+let a=10;
+console.log(typeof(a));
+let b=20;
+console.log(typeof(b));
+let c=99999999999999999n;
+console.log(typeof(c));
+
+let d='s'
+console.log(typeof(d));
+let e ="hello"
+console.log(typeof(e));
+let name = "raj"
+let f=`bye ${name}`
+console.log(f);
+console.log(typeof(f));
+
+let g=true
+console.log(typeof(g));
+
+let h;
+console.log(typeof(h));
+
+let i = null
+console.log(typeof(i));
+
+let j = [10,15.3,"hello"]
+console.log(typeof(j));
+
+let k = {name:"raju"}
+console.log(typeof(k));
+
+
+// array data type
+console.log("----- more on array data type ---");
+let m = [10,20,[55,66],30,{age:30,marks:99}]
+console.log(m.length);
+console.log(m[0]);
+console.log(m[2]);
+console.log(m[2][1]);
+console.log(m[4]);
+console.log(m[4].age);
+console.log(m[7]);
+
+
+
+ 
