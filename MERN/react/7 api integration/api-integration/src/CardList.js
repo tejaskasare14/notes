f82a97e4294dbe46372efebefc9310176474a1f5
tejaskasare14 <tejaskasare.projects@gmail.com>
2023-12-03 11:08:49 +0530

    pagination logic

--- MERN/react/7 api integration/api-integration/src/CardList.js
+++ MERN/react/7 api integration/api-integration/src/CardList.js
@@ -1,26 +1,45 @@
+import { useState } from "react"
 import Card from "./Card"
 import employee from './emp-data.json'
+
 function CardList()
 {
-   let page_no = 0
+   // let page_no = 0
+   let [page_no, setPageNumber] = useState(0)
    let per_page = 3
    let start_index = 0
    let end_index = 3
-   let employees = employee.slice(start_index,end_index)
+   // let employees = employee.slice(start_index,end_index)
+   let [employees,setEmployees]=useState(employee.slice(start_index,end_index))
 
-   let pre_page = () =>{
-         page_no-=1
-         if(page_no>=0)
+   let previous_page = () =>{
+        
+         if(page_no>0)
          {
+            setPageNumber(page_no-=1)
             start_index = page_no * per_page
             end_index = start_index + per_page
             console.log(start_index,end_index);
-            employee = employee.slice(start_index,end_index)
-         }
-         
+            setEmployees(employee.slice(start_index,end_index))
+         }     
    }
+
+   let next_page = () =>{
+      
+      if(page_no<(Math.floor(employee.length/per_page)))
+      {
+         setPageNumber(page_no+=1)
+         start_index = page_no * per_page
+         end_index = start_index + per_page
+         console.log(start_index,end_index);
+         setEmployees(employee.slice(start_index,end_index))
+      }     
+}
    return(
       <>
+         <p>Current Page {page_no+1}</p>
+         <button onClick={previous_page} disabled={page_no==0 || page_no<0?true:false}>Previous </button>
+         <button onClick={next_page}>Next </button>
          {/* sending data to card using array index */}
          {/* 
          <Card employee_data={employee[0]}/>
@@ -36,8 +55,7 @@ function CardList()
                   return <Card {...employee} key={employee.id}/>
                })
          }
-         <button onClick={pre_page}>Previous</button>
-         <button>Next</button>
+        
       </>
    )
 }
