4e7ff43c2f502ab4e862893b240f853ff7e91ffb
tejaskasare14 <tejaskasare.projects@gmail.com>
2023-12-10 11:13:49 +0530

    react with category based filter

--- MERN/react/8 e-commerce app/shopify/src/components/products/ProductList.js
+++ MERN/react/8 e-commerce app/shopify/src/components/products/ProductList.js
@@ -7,16 +7,17 @@ export function ProductList()
 {
 
    let [products,setProducts] = useState([])  
+   let [url, setUrl] = useState("http://localhost:3000/products")
    
    useEffect(()=>{
-      fetch("http://localhost:3000/products")
+      fetch(url)
       .then(response => response.json())
       .then(data =>
          {
             original_products=data
             setProducts(data)
          })
-   },[])
+   },[url])
 
    return(
       <>
@@ -30,18 +31,32 @@ export function ProductList()
                      )
                   })
                } */}
-         <p>ProductList</p>
+        
          <div className="container">
-            <div className="row">                                                          
-               {
-                  products.map(product => {
-                     return (
-                           <Products {...product} key={product.id}/>
-                     )
-                  })
-               }
+               <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                     <input type="radio" class="btn-check" name="btnradio" id="btnradio1" onClick={()=>setUrl("http://localhost:3000/products")}/>
+                     <label class="btn btn-outline-primary" for="btnradio1">All</label>
 
-            </div>
+                     <button onClick={()=>setUrl("http://localhost:3000/products")}>All</button>
+
+                     <input type="radio" class="btn-check" name="btnradio" id="btnradio2"  onClick={()=>setUrl("http://localhost:3000/products?category=mobile")}/>
+                     <label class="btn btn-outline-primary" for="btnradio2">Mobile</label>
+
+                     <input type="radio" class="btn-check" name="btnradio" id="btnradio3" onClick={()=>setUrl("http://localhost:3000/products?category=tv")}/>
+                     <label class="btn btn-outline-primary" for="btnradio3">TV</label>
+
+                     <input type="radio" class="btn-check" name="btnradio" id="btnradio4" onClick={()=>setUrl("http://localhost:3000/products?category=fridge")}/>
+                     <label class="btn btn-outline-primary" for="btnradio4">Fridge</label>
+               </div>
+               <div className="row">                                                          
+                  {
+                     products.map(product => {
+                        return (
+                              <Products {...product} key={product.id}/>
+                        )
+                     })
+                  }
+               </div>
          </div>
       </>
    )
