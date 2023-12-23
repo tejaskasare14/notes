45f98b0487d9b9681a8331a54be5a5bc589f486f
tejaskasare14 <tejaskasare.projects@gmail.com>
2023-12-23 12:03:32 +0530

    react form

--- MERN/react/9 forms/form/src/Form.js
+++ MERN/react/9 forms/form/src/Form.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 export default function Form() {
   let [formData,setFormData]=useState({
-    name:'',
-    email:''
+    username:'',
+    useremail:'',
+    usercourse:'java'
   })
    const captureForm=(event)=>
    {
@@ -21,13 +22,19 @@ export default function Form() {
     <div>
       <form onSubmit={submitForm}>
          <label htmlFor="username">Name:</label>
-         <input type="text" id="username" value={formData.name} name="name" onChange={captureForm}/>
-
-         <br /><br />
+         <input type="text" id="username" value={formData.username} name="username" onChange={captureForm}/> <br /><br />
 
          <label htmlFor="useremail">Email:</label>
-         <input type="text" id="useremail" value={formData.email} name="email" onChange={captureForm}/>
+         <input type="text" id="useremail" value={formData.useremail} name="useremail" onChange={captureForm}/> <br /><br />
 
+         <label htmlFor="usercourse">Course:</label>
+         <select id="usercourse" value={formData.usercourse} name="usercourse" onChange={captureForm}>
+            <option value="dse">Data Software Enginnering</option>
+            <option value="java">Java</option>
+            <option value="python">Python</option>
+            <option value="ds">Data Science</option>
+          </select> <br /><br />
+         
          <input type="submit" />
 
          <p>Data {JSON.stringify(formData)}</p>
