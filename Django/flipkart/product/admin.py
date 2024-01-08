278035ac8b5d3fb8b0bdcdd4ef414c6979ec99b1
tejaskasare14 <tejaskasare.projects@gmail.com>
2024-01-08 15:07:46 +0530

    django mysql

--- Django/flipkart/product/admin.py
+++ Django/flipkart/product/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from product.models import ProductTable
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+   list_display= ["id","name", "price"]
+
+admin.site.register(ProductTable,ProductAdmin)
--- Django/flipkart/product/models.py
+++ Django/flipkart/product/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class ProductTable(models.Model):
+   name = models.CharField(max_length=20)
+   price = models.IntegerField()
+   
+   def __str__(self) :
+      return "product " + self.name +" added successfully"
