dd4d8eb5e0838428886131b0c00bf4b04b2d672b
tejaskasare14 <tejaskasare.projects@gmail.com>
2024-01-24 13:05:01 +0530

    js datatype and operators

---
+++ Web Development/JS/datatype.js
@@ -0,0 +1,45 @@
+let a = 10;
+console.log(a);
+console.log(typeof(a));
+
+let b = 10.55;
+console.log(b);
+console.log(typeof(b));
+
+let c = 'javascript';
+console.log(c);
+console.log(typeof(c));
+
+let d = "html";
+console.log(d);
+console.log(typeof(d));
+
+let username ="rani"
+let marks = 96
+let e = `hello ${username}, you have scored ${marks}`
+console.log(e);
+console.log(typeof(e));
+
+let f;
+console.log(f);
+console.log(typeof(f));
+
+let g=null;
+console.log(g);
+console.log(typeof(g));
+
+let arr=[40,40.3,"hello"];
+console.log(arr);
+console.log(typeof(arr));
+console.log(arr[0]);
+console.log(arr[5]);
+
+let obj = {sql:95,py:99}
+console.log(obj);
+console.log(typeof(obj));
+console.log(obj.sql);
+console.log(obj.html);
+
+let i = true
+console.log(i);
+console.log(typeof(i));
---
+++ Web Development/JS/operator.js
@@ -0,0 +1,74 @@
+// arithamtic
+console.log(10+2);
+console.log(10-2);
+console.log(10*2);
+console.log(10/2);
+console.log(10.0/2);
+console.log(10/3);
+console.log(10**2);
+console.log(10%2);
+
+//pre and post increament
+//pre ++x: first do increment then print
+//post x++ - first print and do increment for next use
+let x = 10
+console.log(++x);
+console.log(x);
+
+let y = 20
+console.log(y++);
+console.log(y);
+
+let a = 10
+console.log(--a);
+console.log(a);
+
+let b = 20
+console.log(b--);
+console.log(b);
+
+//assignment
+let c = 30
+console.log(c);
+
+c = c + 5 //30+5
+console.log(c);
+
+c +=5 //c = c + 5 => c = 35+5
+console.log(c);
+
+c-=10
+console.log(c);
+
+//comparision 
+console.log(10>5);
+console.log(10<5);
+console.log(10>=10);
+console.log(10<=10);
+console.log(10!=12);
+console.log(10==10);
+// == is used for only data comparision
+// for == data type doenst matter
+console.log(5=="5");
+//=== is used for both datatype and data comparision
+console.log(5==="5");
+
+//ternary
+//condition?msg if condition is true:msg if condition is false
+console.log(10>50?"yes it is":"no it in not");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
